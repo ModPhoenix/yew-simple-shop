509ef0352291dfc19f27513e42792e4599156228
ModPhoenix <ivanchenko.b@gmail.com>
2021-01-24 23:33:07 +0200

    Initial project setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,38 @@
-const path = require('path');
-const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const distPath = path.resolve(__dirname, "dist");
 module.exports = (env, argv) => {
   return {
     devServer: {
       contentBase: distPath,
-      compress: argv.mode === 'production',
-      port: 8000
+      compress: argv.mode === "production",
+      port: 8000,
     },
-    entry: './bootstrap.js',
+    entry: "./bootstrap.js",
     output: {
       path: distPath,
-      filename: "todomvc.js",
-      webassemblyModuleFilename: "todomvc.wasm"
+      filename: "bundle.js",
+      webassemblyModuleFilename: "bundle.wasm",
     },
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
-          ],
+          use: ["style-loader", "css-loader", "sass-loader"],
         },
       ],
     },
     plugins: [
       new CopyWebpackPlugin({
-        patterns: [
-          { from: './static', to: distPath },
-        ],
+        patterns: [{ from: "./static", to: distPath }],
       }),
       new WasmPackPlugin({
         crateDirectory: ".",
         extraArgs: "--no-typescript",
-      })
+      }),
     ],
-    watch: argv.mode !== 'production'
+    watch: argv.mode !== "production",
   };
 };
